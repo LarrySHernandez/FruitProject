1213832b23e383d0ec72c9415d5c21c4d1e2dd77
Larry <larryh2612@gmail.com>
2024-09-05 13:37:29 -0400

    Version 1.0

---
+++ script.js
@@ -0,0 +1,39 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+
+
+
+function search(str) {
+	let results = [];
+	results = fruit.filter((val) => (val.toLowerCase()).indexOf(str) !== 1);
+	console.log(results);
+	return results;
+	
+}
+
+function searchHandler(e) {
+	input.value === ''? suggestions.innerHTML = '':showSuggestions(search(input.value), input.value);
+}
+
+function showSuggestions(results, inputVal) {
+	suggestions.innerHTML = '';
+	results.slice(0,5).forEach((val) => {
+		let newLi = document.createElement('li');
+		// newLi.innerHTML = val.replace(inputVal, `<b>${inputVal}</b>`);
+		newLi.innerText = val;
+		newLi.classList.add('ul');
+		suggestions.append(newLi);
+		});
+}
+
+function useSuggestion(e) {
+	input.value = e.srcElement.innerText;
+	suggestions.innerHTML = '';
+
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
